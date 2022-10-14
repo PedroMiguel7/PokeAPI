b1ba90e27fe4d4297e27489823c45e9bd7422fd2
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-10-14 10:45:23 -0300

    feat: creat and config form

---
+++ src/app/components/theme-button/theme-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-button',
+  templateUrl: './theme-button.component.html',
+  styleUrls: ['./theme-button.component.scss']
+})
+export class ThemeButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
+import { LoginService } from './../../services/login.service';
+import { USER } from './user.interface';
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  user?: USER
 
-  constructor() { }
+  constructor(private LoginService: LoginService, private fb: UntypedFormBuilder) { }
 
+  validateForm!: UntypedFormGroup
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      email: [],
+      password: []
+    })
   }
 
+  fazLogin() {
+    console.log('fazLogin');
+    console.log(this.validateForm.value);
+  }
 }
