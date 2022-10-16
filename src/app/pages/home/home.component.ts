c16e79436c18110f003ffa3fdd5fb889994d3f02
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-10-16 17:34:25 -0300

    feat: page all, initial struct

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  ativo1: boolean = true;
+  ativo2: boolean = false;
+  ativo3: boolean = false;
+  ativo4: boolean = false;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mostraTodos(){
+    this.ativo1 = !this.ativo1;
+    this.ativo2 = false;
+    this.ativo3 = false;
+    this.ativo4 = false;
+  }
+
+  mostraFire(){
+    this.ativo1 = false;
+    this.ativo2 = !this.ativo2;
+    this.ativo3 = false;
+    this.ativo4 = false;
+  }
+
+  mostraEletric(){
+    this.ativo1 = false;
+    this.ativo2 = false;
+    this.ativo3 = !this.ativo3;
+    this.ativo4 = false;
+  }
+
+  mostraWater(){
+    this.ativo1 = false;
+    this.ativo2 = false;
+    this.ativo3 = false;
+    this.ativo4 = !this.ativo4;
+  }
 }
