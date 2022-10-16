4992ad17b8f115d394186ea3ef842d8aaa582afa
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-10-16 12:56:57 -0300

    feat: button to change theme

--- src/app/components/theme-button/theme-button.component.ts
+++ src/app/components/theme-button/theme-button.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./theme-button.component.scss']
 })
 export class ThemeButtonComponent implements OnInit {
+  public tema: Array <string> = ["moon", "DARK"];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public toggle(){
+    const theme = document.body.classList.toggle('dark-theme');
+    if(theme){
+      return this.tema = ["sun", "LIGHT"];
+    }
+    return this.tema = ["moon", "DARK"];
   }
 
 }
