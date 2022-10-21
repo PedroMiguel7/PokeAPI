59fdc0ea69a4a62de539e87e0edb8c292405318c
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-10-21 11:51:05 -0300

    feat: figth(pikachu)

--- src/app/components/pokeboll/pokeboll.component.ts
+++ src/app/components/pokeboll/pokeboll.component.ts
@@ -8,6 +8,8 @@ import { timeout } from 'rxjs';
 export class PokebollComponent implements OnInit {
 
   Pokebola?: boolean = true;
+  Pokebola_abre?: boolean = false;
+  Pokemon?: boolean = false;
 
   constructor() { }
 
@@ -16,6 +18,12 @@ export class PokebollComponent implements OnInit {
     setTimeout(
       () => {
         this.Pokebola = false;
+        this.Pokebola_abre = true;
+        setTimeout( () => {
+          this.Pokemon = true;
+          this.Pokebola_abre = false;
+        }, 1100
+        )
       }, 3000)
   }
 
