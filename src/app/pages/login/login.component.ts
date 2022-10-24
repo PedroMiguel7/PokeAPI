ea4bced7d9ddf43ec434c06a6afc0820f54c3582
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-10-24 12:01:48 -0300

    feat: new imagens

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,15 +4,34 @@ import { USER } from './user.interface';
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0
+  }),
+  animate('1s ease-in', style({opacity: 1})),
+])
+const FadeIn = trigger('fadeIn', [enterTransition])
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [FadeIn]
 })
+
+
 export class LoginComponent implements OnInit {
   user?: USER
   lU?: loginUSer
+
+  isShow = false;
+  isShowButton = true;
+  
+  fadeInOut(): void {
+    this.isShow = !this.isShow
+    this.isShowButton = false;
+  }
 
   constructor(private router: Router, private LoginService: LoginService, private fb: UntypedFormBuilder) { }
 
