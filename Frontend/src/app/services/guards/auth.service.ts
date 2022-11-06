e0ec6e859a546d96d144133798488615fef29d41
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-11-05 22:45:28 -0300

    fix: service auth guard

--- Frontend/src/app/services/guards/auth.service.ts
+++ Frontend/src/app/services/guards/auth.service.ts
@@ -7,15 +7,12 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 })
 export class AuthService {
   public LoginResponse?: LoginService;
-
   public clear(): void {
     this.LoginResponse = undefined;
   }
-
   public isAuthenticated(): boolean {
     const helper = new JwtHelperService();
     const Token = JSON.stringify(localStorage.getItem('access_token'));
-
     const decodedToken = helper.decodeToken(Token);
     return Boolean(this.LoginResponse);
   }
