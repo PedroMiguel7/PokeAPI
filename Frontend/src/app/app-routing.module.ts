196e92a4281b627d903c9e82b38d6f73536cf63e
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-11-26 22:59:10 -0300

    fix: autorization

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { GuardaAuthService } from './services/guards/guarda-auth.service';
+// import { GuardaAuthService } from './services/guards/guarda-auth.service';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { SearchComponent } from './pages/search/search.component';
 
