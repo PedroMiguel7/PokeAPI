ffeeb231386f6a872a3b93a99a2e3c15d0d420d2
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2023-01-04 11:58:35 -0300

    feat: criando componente, modulo e rotas do favoritos

---
+++ Frontend-Refatorado/src/app/pages/favoritos/favoritos.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FavoritosRoutingModule } from './favoritos-routing.module';
+import { FavoritosComponent } from './componet/favoritos.component';
+
+@NgModule({
+  declarations: [FavoritosComponent],
+  imports: [CommonModule, FavoritosRoutingModule],
+})
+export class FavoritosModule {}
