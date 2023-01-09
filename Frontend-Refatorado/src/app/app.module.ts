adc2fc03153deecefef51b53253d72e7b902be5a
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2023-01-09 20:46:27 -0300

    feat: criando rota e componente de login

--- Frontend-Refatorado/src/app/app.module.ts
+++ Frontend-Refatorado/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './pages/login/login.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, LoginModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Frontend-Refatorado/src/app/pages/login/login.module.ts
+++ Frontend-Refatorado/src/app/pages/login/login.module.ts
@@ -3,14 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import { LoginComponent } from './login-componente/login.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SharedModule,
-    LoginRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, SharedModule, LoginRoutingModule],
 })
-export class LoginModule { }
+export class LoginModule {}
