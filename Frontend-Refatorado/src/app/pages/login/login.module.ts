46c29a986b96db176c1dbf7cb213c2df3864bb12
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2023-01-11 14:42:24 -0300

    fix: removendo modulo de rota de login

--- Frontend-Refatorado/src/app/pages/login/login.module.ts
+++ Frontend-Refatorado/src/app/pages/login/login.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoginRoutingModule } from './login-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginComponent } from './login-componente/login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, SharedModule, LoginRoutingModule],
+  imports: [CommonModule, SharedModule],
 })
 export class LoginModule {}
